27e5aae54d8a639e9d3e9b7093ea443dda23323e
liaoyu <449540412@qq.com>
2025-11-26 13:45:54 +0800

    first commit

---
+++ FSDaySchedule/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  FSDaySchedule
+//
+//  Created by liaoyu on 2025/11/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
