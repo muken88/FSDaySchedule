95963e7c853d54aa5da32b433a87f1d7748fb044
muken88 <449540412@qq.com>
2025-11-26 13:45:54 +0800

    first commit

---
+++ FSDaySchedule/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  FSDaySchedule
+//
+//  Created by liaoyu on 2025/11/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FSDaySchedule/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  FSDaySchedule
+//
+//  Created by liaoyu on 2025/11/26.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"123");
+    
+    // Do any additional setup after loading the view.
+}
+
+
+@end
